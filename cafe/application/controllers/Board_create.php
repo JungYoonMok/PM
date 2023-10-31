eb97fe5f8214f31cc521f4d90d49a2cce2f45b3d
YoonMok <po_za@naver.com>
2023-10-31 22:40:12 +0900

    ci - cafe - 2310312040

--- cafe/application/controllers/Board_create.php
+++ cafe/application/controllers/Board_create.php
@@ -19,8 +19,53 @@
       $this->load->view('footer');
     }
 
+    public function store_file()
+    {
+      $config['upload_path'] = './uploads';
+      $config['allowed_types'] = 'gif|jpg|png';
+      $config['max_size'] = '0';
+      $config['max_width'] = '0';
+      $config['max_height'] = '0';
+      // $config['file_name'] = date("ymd") . '_' . date("His"); // 이미지 파일명 변환
+      $this->load->library('upload', $config);
+      
+      if( ! $this->upload->do_upload("upload") ) {
+        echo "<script>alert('업로드에 실패 했습니다. ".$this->upload->display_errors('','')."')</script>";
+      } else {
+        $data = array('upload_data' => $this->upload->data());
+        echo "성공";
+        var_dump($data);
+        
+        // return new JsonResponse([
+        //   'uploaded' => true,
+        //   'fileName' => $image,
+        //   // 'url' => $url
+        //   'url' => './uploads/test.png'
+        // ], 200);
+
+      }
+    }
+
     public function store()
     {
+      // $config['upload_path'] = './uploads';
+      // $config['allowed_types'] = 'gif|jpg|png';
+      // $config['max_size'] = '0';
+      // $config['max_width'] = '0';
+      // $config['max_height'] = '0';
+      // // $config['file_name'] = date("ymd") . '_' . date("His"); // 이미지 파일명 변환
+      // $this->load->library('upload', $config);
+      
+      // if( ! $this->upload->do_upload("upload") ) {
+      //   echo "<script>alert('업로드에 실패 했습니다. ".$this->upload->display_errors('','')."')</script>";
+      // } else {
+      //   $data = array('upload_data' => $this->upload->data());
+      //   echo "성공";
+      //   var_dump($data);
+      // }
+    
+
+
       // 폼 벨리데이션으로 폼의 필수값을 지정
       $this->form_validation->set_rules('title', 'Title', 'required');
       $this->form_validation->set_rules('contents', 'Contents', 'required');
--- cafe/application/views/Board/free_board_create.php
+++ cafe/application/views/Board/free_board_create.php
@@ -17,22 +17,26 @@
         <h2>자유게시판 - 게시글 등록</h2>
       </div>
 
-      <div class="bg-[#2f2f2f] border border-gray-500 p-10 rounded flex flex-col gap-5 relative drop-shadow-2xl">
+      <div class="bg-[#2f2f2f] border border-gray-500 p-5 rounded flex flex-col gap-5 relative drop-shadow-2xl">
 
-        <div>
+          <!-- CKEditor 사용시 form에 enctype="multipart/form-data" 작성하지 않아도 된다. -->
+          <form class="" action="/board_create/store" method="post"">
 
-          <form class="h-full" action="/board_create/store" method="post">
-
-            <div class="flex flex-col h-full gap-3">
+            <div class="flex flex-col h-full w-full gap-3">
               <h2>제목</h2>
               <input class="rounded outline-none duration-200 p-3 bg-[#3f3f3f] border border-slate-400 focus:border-slate-500 focus:bg-[#2f2f2f]" type="text" name="title" value="" />
-              <div class="text-black h-[300px]">
+              <div class="">
                 <textarea id="editor" name="contents"></textarea>                
               </div>
               <!-- <textarea class="rounded outline-none duration-200 p-3 bg-[#3f3f3f] border border-slate-400 focus:border-slate-500 focus:bg-[#2f2f2f]" name="contents" rows="8" value=""></textarea> -->
-              <input class="p-3 bg-blue-500 rounded cursor-pointer hover:opacity-80 duration-200" type="submit" value=" 저장 " />
+              <div class="flex justify-between place-items-center">
+                <div>
+                  <p class="">CK Editor -</p>
+                </div>
+                <input class="w-[30%] p-3 bg-blue-500 rounded cursor-pointer hover:opacity-80 duration-200" type="submit" value=" 저장 " />
+              </div>
             </div>
-            
+          
           </form>
           
         </div>
@@ -46,15 +50,29 @@
 </div>
 
 <style>
-  .ck-editor__editable { height: 300px; }
+  .ck-editor__editable { height: 40rem; color : black; }
 </style>
 
 <script>
 
-  ClassicEditor
-    .create( document.querySelector( '#editor' ) )
-    .catch( error => {
-        console.error( error );
-    } );
+  var myEditor;
+
+  ClassicEditor.create(document.querySelector('#editor'),
+  {
+
+    ckfinder: {
+      uploadUrl: '/board_create/store_file',
+      // openerMethod: 'popup'
+    }
+
+  })
+  .then( editor => {
+    console.log( 'Editor was initialized', editor );
+    myEditor = editor;
+  })
+  .catch( err => {
+    console.log('에러다', err );
+    }
+  );
 
 </script>
